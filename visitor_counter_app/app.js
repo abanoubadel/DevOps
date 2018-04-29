b4e1efed15cd84a30591a32742584c3462a4fa12
Abanoub Adel <abanoub.adel@spimesenselabs.com>
2018-04-29 12:29:14 +0200

    Added visitor counter app

---
+++ visitor_counter_app/app.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const redis = require('redis');
+
+const redisClinet = redis.createClient();
+
+const server = http.createServer(function (req, res) {
+
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        res.end();
+        return;
+    }
+
+    redisClinet.incr('visits', function (e, n) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(`Hello From ${process.env.NAME} \n\n`);
+        res.write(`You are visitor #${n}`);
+        console.log(`visitor counter ${n}`);
+        res.end();
+    });
+
+});
+
+server.listen(8000, function () {
+    console.log(`Server Started at http://${server.address().address}:${server.address().port}`);
+});
